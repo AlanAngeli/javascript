67094a9ce74173feeaa9f2bddc231ab7179f6b23
Alan <71236097+AlanAngeli@users.noreply.github.com>
2022-06-09 03:54:51 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 alert('Tudo certo, jovem padawan!')
+
+//boolean
+var vOuF = false;
+console.log(typeof(vOuF));
+
+//number
+var = numero = 1;
+console.log(typeof(numero));
+
+//string
+var = nome = 1;
+console.log(typeof(nome));
